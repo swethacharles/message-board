4ac4eeb81930e913c7862f90f2f39b894c81fab9
Swetha Charles <swethacharles10@gmail.com>
2021-03-15 19:27:25 +0000

    Added method for submitting a message

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-
+const messages: string[] = [];
 app.get('/getMessages', (_, response) => {
     // TODO add message variable
     response.send('somebody to lean on - bill withers');
@@ -16,6 +16,11 @@ app.listen(PORT, () => {
 
 
 // TODO create method to post message
+app.post('/submitMessage', (request, response) => {
+    messages.push(request.body); // TODO what if it's empty
 
+    response.status(200).send('Thanks for your message!');
+});
 
 // TODO write tradeoffs
+
