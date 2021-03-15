bf483b2bd4db3d8ce66f9180888a370bc6676ea1
Swetha Charles <swethacharles10@gmail.com>
2021-03-15 19:21:53 +0000

    Added method to get messages - hardcoded message variable to test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-// TODO create method to post  message
+import express from "express";
 
-// TODO create method to get messages
+const app = express();
+const PORT = 3000;
+
+
+app.get('/getMessages', (_, response) => {
+    // TODO add message variable
+    response.send('somebody to lean on - bill withers');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Started app - listening at ${PORT}`)
+});
+
+
+// TODO create method to post message
+
 
 // TODO write tradeoffs
