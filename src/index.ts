c867e72c9d4bb3184d17c17736d289804f80a513
Swetha Charles <swethacharles10@gmail.com>
2021-03-15 19:54:24 +0000

    Removed finished TODOs and re-formatted

--- src/index.ts
+++ src/index.ts
@@ -13,19 +13,17 @@ app.get('/getMessages', (_, response) => {
   response.send(JSON.stringify({ messages }));
 });
 
-app.listen(PORT, () => {
-  console.log(`Started app - listening at ${PORT}`);
-});
-
 app.post('/submitMessage', (request, response) => {
   const parsedMessage = parseSubmitMessageRequest(request);
 
   if (parsedMessage) {
-    messages.push(parsedMessage); // TODO check if valid
+    messages.push(parsedMessage);
     response.status(200).send('Thanks for your message!');
   } else {
     response.status(400).send('Could not parse request. Please submit messages in the form { message: <non-empty-message-here> }');
   }
 });
 
-// TODO write tradeoffs
+app.listen(PORT, () => {
+  console.log(`Started app - listening at localhost:${PORT}`);
+});
